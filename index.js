9eb484b7c13ea39a07725d423d2d8b49d34fa761
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-11-09 02:33:40 +0600

    Review post and get check

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
 require('dotenv').config();
+
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bz2bbta.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -13,7 +15,9 @@ const run = async () => {
   try {
     const serviceCollection = client.db("allservices").collection("services");
     const blogsCollection = client.db('blogs').collection('newblog');
-    app.get('/services',async(req,res)=>{
+    const serviceReviewsCollection = client.db('serviceReview').collection('reviews');
+
+    app.get('/services', async (req, res) => {
       const query = {};
       const size = parseInt(req.query.size);
       const cursor = serviceCollection.find(query);
@@ -21,12 +25,35 @@ const run = async () => {
       res.send(result);
     })
 
-    app.get('/blog', async(req,res)=>{
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    })
+
+    app.get('/blog', async (req, res) => {
       const query = {};
       const cursor = blogsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.post('/reviews', async (req, res) => {
+      const review = req.body;
+      const result = await serviceReviewsCollection.insertOne(review);
+      console.log(`Inserted with the _id: ${result.insertedId}`);
+      res.send(result);
+      // console.log(review)
+    })
+
+    app.get('/reviews',async(req,res)=>{
+      const query = {};
+      const cursor = await serviceReviewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
 
   } finally {
 
