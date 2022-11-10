30312666c7c7d9127c92739331ef2ed8075e304f
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-11-10 13:54:33 +0600

    JWT check

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
@@ -10,6 +11,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bz2bbta.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 const run = async () => {
   try {
@@ -26,7 +46,7 @@ const run = async () => {
     })
 
 
-    app.post('/services',async(req,res)=>{
+    app.post('/services', async (req, res) => {
       let serviceObj = req.body;
       serviceObj.date = new Date();
       const result = await serviceCollection.insertOne(serviceObj);
@@ -57,10 +77,24 @@ const run = async () => {
       res.send({ result, review });
     })
 
-    app.get('/reviews', async (req, res) => {
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ token })
+    })
+
+    app.get('/reviews', verifyJWT, async (req, res) => {
+
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.user_email) {
+        res.status(403).send({ message: 'unauthorized access' })
+      }
+
       let query = {};
+      
       const service_id_query = req.query.service_id;
       const email_query = req.query.user_email;
+
       if (service_id_query) {
         query = { service_id: service_id_query };
       }
